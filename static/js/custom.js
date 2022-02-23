a7df8adc9eff6c7a5a6075902af650398fe6595d
llxlr <i@xhlr.top>
2022-02-23 17:32:09 +0800

    ok

---
+++ static/js/custom.js
@@ -0,0 +1,27 @@
+console.log(`             -. .
+       _____   ',' ,
+     ,'     ,'   ', ',
+   ,'     ,'      |  |
+   \\       \\       |  |
+     \\ /^\\   \\    ,' ,'
+           \\   \\ ,' ,'      L'Internationale,
+     / ~-.___\\.-'  ,'            Sera le genre humain.
+   /   .______.- ~ \\
+ /   /'          \\   \\
+ \\./               \\/'`);
+
+!(function() {
+  let cdn = document.getElementById('cdn');
+  if(!cdn){return;}
+  fetch('/cdn-cgi/trace').then(resp => resp.text()).then(res => {
+    let area = res.split('colo=')[1].split('\\n')[0];
+    fetch('https://cdn.jsdelivr.net/gh/llxlr/cdn/static/areas.json').then(resp => resp.json()).then(data => {
+      for(var i in data){
+        if (data[i].colo == area) {
+          let info = data[i].city+', '+data[i].region;
+          console.log(info);cdn.innerHTML = info;
+        }
+      }
+    })
+  })
+})();
