7f2a4ffb2b342fa8e62e344b36ad5769b66ef692
llxlr <i@xhlr.top>
2022-03-11 16:09:36 +0800

    ❤️

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,28 +1,10 @@
-console.log(`             -. .
-       _____   ',' ,
-     ,'     ,'   ', ',
-   ,'     ,'      |  |
-   \\       \\       |  |
-     \\ /^\\   \\    ,' ,'
-           \\   \\ ,' ,'      L'Internationale,
-     / ~-.___\\.-'  ,'            Sera le genre humain.
-   /   .______.- ~ \\
- /   /'          \\   \\
- \\./               \\/'
-`);
-
-!(function() {
-  let cdn = document.getElementById('cdn');
-  if(!cdn){return;}
-  fetch('https://white-album.top/cdn-cgi/trace').then(resp => resp.text()).then(res => {
-    let area = res.split('colo=')[1].split('\\n')[0];
-    fetch('https://cdn.jsdelivr.net/gh/llxlr/cdn/static/areas.json').then(resp => resp.json()).then(data => {
-      for(var i in data){
-        if (data[i].colo == area) {
-          let info = data[i].city+', '+data[i].region;
-          console.log(info);cdn.innerHTML = info;
-        }
-      }
-    })
-  })
-})();
+window.onload = function(){
+  function switchCDN(origin, cdn){document.body.innerHTML = document.body.innerHTML.replace(origin, cdn);}
+  try {
+    if (returnCitySN.cid !== 'CN') {
+      switchCDN(/s-bj-92-blog\.oss\.dogecdn\.com/g, 'cdn.white-album.top');
+    }
+  } catch {
+    switchCDN(/cdn\.white-album\.top/g, 's-bj-92-blog.oss.dogecdn.com');
+  }
+}
