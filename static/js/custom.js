24e46f96347cfba788880c1e7b9df8e84508d84d
llxlr <i@xhlr.top>
2022-03-17 12:44:30 +0800

    ok

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,15 +2,17 @@ const switchCDN = function(){
   try {
     if (returnCitySN.cid !== 'CN') {
       document.body.innerHTML = document.body.innerHTML.replace(/s-bj-92-blog\.oss\.dogecdn\.com/g, 'cdn.white-album.top');
+    } else {
+      document.body.innerHTML = document.body.innerHTML.replace(/cdn\.white-album\.top/g, 's-bj-92-blog.oss.dogecdn.com');
     }
-  } catch {
-    document.body.innerHTML = document.body.innerHTML.replace(/cdn\.white-album\.top/g, 's-bj-92-blog.oss.dogecdn.com');
+  } catch(e) {
+    console.log(e);
   }
 }
 
 document.addEventListener('pjax:send', function(){
   switchCDN();
 });
-document.addEventListener('load', function(){
-  switchCDN();
-});
+// document.addEventListener('load', function(){
+//   switchCDN();
+// });
